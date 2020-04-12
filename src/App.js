aef744a8ef26b42d87cd54733dbc675097944091
Paul Elvis Tcheumeni <hellotopaul@gmail.com>
2020-04-12 12:41:20 -0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'tachyons';
-import FunctionComp from './components/FunctionComp';
-import ClassComp from './components/ClassComp';
-import MessageComp from './components/Message';
-import CountComp from './components/Counter';
-import FunctionClickEvent from './components/FunctionClickEvent';
-import ClassEventBind from './components/ClassEventBind';
+import FunctionComp from './components/funClassComp/FunctionComp';
+import ClassComp from './components/funClassComp/ClassComp';
+import MessageComp from './components/state/Message';
+import CountComp from './components/setState/Counter';
+import FunctionClickEvent from './components/funClassEventBind/FunctionClickEvent';
+import ClassEventBind from './components/funClassEventBind/ClassEventBind';
 import ChildToParent from './components/childToParent/ParentComp';
 import ConditionRender from './components/conditionRender/Condition';
 import PersonList from './components/itemList/PersonList';
@@ -19,25 +19,17 @@ import Form from './components/form/form';
 import MountingMethod from './components/lifecycles/mountingMethod/MountingA';
 import UpdatingMethod from './components/lifecycles/updatingMethod/UpdatingA';
 import Fragment from './components/fragments/Fragments';
-import ParentComp from './components/pureComponents/ParentComp';
-import ParentComps from './components/memoComponent/parentComps';
+import PureComp from './components/pureComponents/ParentComp';
+import MemoComp from './components/memoComponent/parentComps';
 
 class App extends Component {
   render() {
     return (
       <div className="App code tc">
-        {/*
-        <FunctionComp name="propsName1">
-          <p>This is a props</p>
-        </FunctionComp>
-        <FunctionComp name="propsName2">
-          <p>This is a props 2</p>
-          <button>Start</button>
-        </FunctionComp>
-        <ClassComp name="propsName1"/>
-        <ClassComp name="propsName2"/>
-        */}
-
+        {/*<FunctionComp name="propsName1"><p>This is a props</p></FunctionComp>*/}
+        {/*<FunctionComp name="propsName2"><p>This is a props 2</p></FunctionComp>*/}
+        {/*<ClassComp name="propsName1"/>*/}
+        {/*<ClassComp name="propsName2"/>*/}
         {/*<MessageComp></MessageComp>*/}
         {/*<CountComp></CountComp>*/}
         {/*<FunctionClickEvent></FunctionClickEvent>*/}
@@ -56,8 +48,8 @@ class App extends Component {
         {/*<MountingMethod></MountingMethod>*/}
         {/*<UpdatingMethod></UpdatingMethod>*/}
         {/*<Fragment></Fragment>*/}
-        {/*<ParentComp></ParentComp>*/}
-        <ParentComps></ParentComps>
+        {/*<PureComp></PureComp>*/}
+        {/*<MemoComp></MemoComp>*/}
       </div>
     );
   }
